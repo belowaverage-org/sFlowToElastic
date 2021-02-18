6945d29045b84716125af0a3b438ac3f2c2d7811
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2021-02-18 17:01:48 -0500

    Decoding counter records start.

--- sFlowLibrary/Classes/sFlow/Samples/Counter/CounterSample.cs
+++ sFlowLibrary/Classes/sFlow/Samples/Counter/CounterSample.cs
@@ -1,4 +1,5 @@
 ﻿using BelowAverage.sFlow.Samples.Counter.Records;
+using System;
 
 namespace BelowAverage.sFlow.Samples.Counter
 {
@@ -8,6 +9,22 @@ namespace BelowAverage.sFlow.Samples.Counter
         public CounterSample(byte[] buffer) : base(buffer)
         {
             Records = new CounterRecord[buffer.ToUInt(16, 4)];
+            uint recordStartIndex = 20;
+            for (uint i = 0; i < Records.Length; i++)
+            {
+                CounterRecord record = new CounterRecord(buffer.AsSpan((int)recordStartIndex, (int)Record.HeaderLengthBytes).ToArray());
+                int recordLength = (int)(record.Length + Record.HeaderLengthBytes);
+                /*if (record.Type == RecordType.asdf)
+                {
+                    record = new RawPacketHeader(buffer.AsSpan((int)recordStartIndex, recordLength).ToArray());
+                }
+                else if (record.Type == RecordType.ExtSwitchData)
+                {
+                    record = new SwitchData(buffer.AsSpan((int)recordStartIndex, recordLength).ToArray());
+                }*/
+                recordStartIndex += (uint)recordLength;
+                Records[i] = record;
+            }
         }
     }
 }
